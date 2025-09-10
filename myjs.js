a42324ebdc4fc49bbb10105c8534698f24414b9b
shuyu07150715-sketch <shuyu07150715@gmail.com>
2025-09-10 20:54:01 +0800

    upload all file
    上傳所有檔案，不包含資料夾

---
+++ myjs.js
@@ -0,0 +1,12 @@
+//雙斜線為js註解
+
+//建立函數，改變標題的色彩
+function changeHead(){ //函數可自行命名-> changeHead() 
+document.getElementById('myh').style.color="pink"; 
+//getElementById -> 取得元素(id->myh)
+}
+
+//建立函數，還原標題的色彩
+function clearHead(){
+document.getElementById('myh').style.color="beige";
+}
